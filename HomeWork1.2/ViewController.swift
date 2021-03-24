1ebac48de75a256d4b0a605aab29b139fdebfcbf
Nikita Neluybin <nelub77@icloud.com>
2021-03-25 02:22:32 +0500

    Application logic

--- HomeWork1.2/ViewController.swift
+++ HomeWork1.2/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    enum Lights {
+        case red, yellow, green
+    }
+    var currentLight = Lights.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.setTitle("START", for: .normal)
+        redView.alpha = 0.3
+        yellowView.alpha = 0.3
+        greenView.alpha = 0.3
+    }
+    override func viewWillLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.height / 2
+        greenView.layer.cornerRadius = greenView.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.height / 2
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("NEXT", for: .normal)
+        switch currentLight {
+        
+        case .red:
+            redView.alpha = 1
+            yellowView.alpha = 0.3
+            greenView.alpha = 0.3
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+            greenView.alpha = 0.3
+            currentLight = .green
+        case .green:
+            redView.alpha = 0.3
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+            currentLight = .red
+        }
+    }
 }
 
